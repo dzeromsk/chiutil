e224df9b28532498ebc512967ab80fae443ea9d2
Dominik Zeromski <dzeromsk@gmail.com>
2023-04-22 19:43:29 +0200

    rename go module

--- example/example.go
+++ example/example.go
@@ -5,8 +5,7 @@ import (
 	"net/http"
 	"time"
 
-	"chiutil"
-
+	"github.com/dzeromsk/chiutil"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
